527423032a0c8756ee255096533410aaab1c7ddd
cavoprync <stepanchomyn@seznam.cz>
2024-01-16 11:27:53 +0100

    komit 16.1.

---
+++ lidlcz/blog/models.py
@@ -0,0 +1,8 @@
+
+from django.conf import settings
+from django.db import models
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
